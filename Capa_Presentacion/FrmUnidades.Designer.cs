1fe4f5a60bc5182b543aebbc50a927f9b97f3197
SISTEMAINTEGRAL <integralramirez@gmail.com>
2020-06-24 17:30:51 -0300

    PRUEBA

--- Capa_Presentacion/FrmUnidades.Designer.cs
+++ Capa_Presentacion/FrmUnidades.Designer.cs
@@ -53,6 +53,7 @@
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.errorIcono = new System.Windows.Forms.ErrorProvider(this.components);
             this.lblUnidades = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.tabLista.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataLista)).BeginInit();
             this.grpboxCategoria.SuspendLayout();
@@ -68,9 +69,10 @@
             // btnNuevo
             // 
             this.btnNuevo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnNuevo.Location = new System.Drawing.Point(38, 145);
+            this.btnNuevo.Location = new System.Drawing.Point(51, 178);
+            this.btnNuevo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnNuevo.Name = "btnNuevo";
-            this.btnNuevo.Size = new System.Drawing.Size(75, 23);
+            this.btnNuevo.Size = new System.Drawing.Size(100, 28);
             this.btnNuevo.TabIndex = 6;
             this.btnNuevo.Text = "Nuevo";
             this.btnNuevo.UseVisualStyleBackColor = true;
@@ -85,11 +87,13 @@
             this.tabLista.Controls.Add(this.lblNombre);
             this.tabLista.Controls.Add(this.btnImprimir);
             this.tabLista.Controls.Add(this.btnEliminar);
+            this.tabLista.Controls.Add(this.button1);
             this.tabLista.Controls.Add(this.btnBuscar);
             this.tabLista.Location = new System.Drawing.Point(4, 29);
+            this.tabLista.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabLista.Name = "tabLista";
-            this.tabLista.Padding = new System.Windows.Forms.Padding(3);
-            this.tabLista.Size = new System.Drawing.Size(668, 249);
+            this.tabLista.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabLista.Size = new System.Drawing.Size(893, 314);
             this.tabLista.TabIndex = 0;
             this.tabLista.Text = "Lista";
             this.tabLista.UseVisualStyleBackColor = true;
@@ -106,12 +110,13 @@
             this.dataLista.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataLista.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Eliminar});
-            this.dataLista.Location = new System.Drawing.Point(7, 83);
+            this.dataLista.Location = new System.Drawing.Point(9, 102);
+            this.dataLista.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataLista.MultiSelect = false;
             this.dataLista.Name = "dataLista";
             this.dataLista.ReadOnly = true;
             this.dataLista.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataLista.Size = new System.Drawing.Size(625, 168);
+            this.dataLista.Size = new System.Drawing.Size(833, 207);
             this.dataLista.TabIndex = 11;
             this.dataLista.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataLista_CellContentClick);
             // 
@@ -124,18 +129,20 @@
             // lblTotal
             // 
             this.lblTotal.AutoSize = true;
-            this.lblTotal.Location = new System.Drawing.Point(557, 61);
+            this.lblTotal.Location = new System.Drawing.Point(743, 75);
+            this.lblTotal.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblTotal.Name = "lblTotal";
-            this.lblTotal.Size = new System.Drawing.Size(13, 13);
+            this.lblTotal.Size = new System.Drawing.Size(16, 17);
             this.lblTotal.TabIndex = 10;
             this.lblTotal.Text = "0";
             // 
             // chkEliminar
             // 
             this.chkEliminar.AutoSize = true;
-            this.chkEliminar.Location = new System.Drawing.Point(25, 60);
+            this.chkEliminar.Location = new System.Drawing.Point(33, 74);
+            this.chkEliminar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.chkEliminar.Name = "chkEliminar";
-            this.chkEliminar.Size = new System.Drawing.Size(94, 17);
+            this.chkEliminar.Size = new System.Drawing.Size(124, 21);
             this.chkEliminar.TabIndex = 9;
             this.chkEliminar.Text = "Eliminar Varios";
             this.chkEliminar.UseVisualStyleBackColor = true;
@@ -143,26 +150,29 @@
             // 
             // txtNombre
             // 
-            this.txtNombre.Location = new System.Drawing.Point(72, 33);
+            this.txtNombre.Location = new System.Drawing.Point(96, 41);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtNombre.Name = "txtNombre";
-            this.txtNombre.Size = new System.Drawing.Size(175, 20);
+            this.txtNombre.Size = new System.Drawing.Size(232, 22);
             this.txtNombre.TabIndex = 8;
             // 
             // lblNombre
             // 
             this.lblNombre.AutoSize = true;
-            this.lblNombre.Location = new System.Drawing.Point(22, 34);
+            this.lblNombre.Location = new System.Drawing.Point(29, 42);
+            this.lblNombre.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblNombre.Name = "lblNombre";
-            this.lblNombre.Size = new System.Drawing.Size(44, 13);
+            this.lblNombre.Size = new System.Drawing.Size(58, 17);
             this.lblNombre.TabIndex = 7;
             this.lblNombre.Text = "Nombre";
             // 
             // btnImprimir
             // 
             this.btnImprimir.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnImprimir.Location = new System.Drawing.Point(507, 30);
+            this.btnImprimir.Location = new System.Drawing.Point(676, 37);
+            this.btnImprimir.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnImprimir.Name = "btnImprimir";
-            this.btnImprimir.Size = new System.Drawing.Size(75, 23);
+            this.btnImprimir.Size = new System.Drawing.Size(100, 28);
             this.btnImprimir.TabIndex = 6;
             this.btnImprimir.Text = "Imprimir";
             this.btnImprimir.UseVisualStyleBackColor = true;
@@ -170,9 +180,10 @@
             // btnEliminar
             // 
             this.btnEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEliminar.Location = new System.Drawing.Point(398, 30);
+            this.btnEliminar.Location = new System.Drawing.Point(531, 37);
+            this.btnEliminar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnEliminar.Name = "btnEliminar";
-            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.Size = new System.Drawing.Size(100, 28);
             this.btnEliminar.TabIndex = 5;
             this.btnEliminar.Text = "Eliminar";
             this.btnEliminar.UseVisualStyleBackColor = true;
@@ -180,9 +191,10 @@
             // btnBuscar
             // 
             this.btnBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnBuscar.Location = new System.Drawing.Point(289, 30);
+            this.btnBuscar.Location = new System.Drawing.Point(385, 37);
+            this.btnBuscar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnBuscar.Name = "btnBuscar";
-            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.Size = new System.Drawing.Size(100, 28);
             this.btnBuscar.TabIndex = 4;
             this.btnBuscar.Text = "Buscar";
             this.btnBuscar.UseVisualStyleBackColor = true;
@@ -191,34 +203,38 @@
             // txtNombreConfig
             // 
             this.txtNombreConfig.Enabled = false;
-            this.txtNombreConfig.Location = new System.Drawing.Point(115, 69);
+            this.txtNombreConfig.Location = new System.Drawing.Point(153, 85);
+            this.txtNombreConfig.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtNombreConfig.Name = "txtNombreConfig";
-            this.txtNombreConfig.Size = new System.Drawing.Size(323, 20);
+            this.txtNombreConfig.Size = new System.Drawing.Size(429, 22);
             this.txtNombreConfig.TabIndex = 14;
             // 
             // txtCodigo
             // 
             this.txtCodigo.Enabled = false;
-            this.txtCodigo.Location = new System.Drawing.Point(115, 23);
+            this.txtCodigo.Location = new System.Drawing.Point(153, 28);
+            this.txtCodigo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtCodigo.Name = "txtCodigo";
-            this.txtCodigo.Size = new System.Drawing.Size(192, 20);
+            this.txtCodigo.Size = new System.Drawing.Size(255, 22);
             this.txtCodigo.TabIndex = 13;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(35, 72);
+            this.label3.Location = new System.Drawing.Point(47, 89);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.Size = new System.Drawing.Size(58, 17);
             this.label3.TabIndex = 11;
             this.label3.Text = "Nombre";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(35, 26);
+            this.label2.Location = new System.Drawing.Point(47, 32);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.Size = new System.Drawing.Size(52, 17);
             this.label2.TabIndex = 10;
             this.label2.Text = "Código";
             // 
@@ -226,9 +242,10 @@
             // 
             this.btnCancelar.Enabled = false;
             this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCancelar.Location = new System.Drawing.Point(439, 145);
+            this.btnCancelar.Location = new System.Drawing.Point(585, 178);
+            this.btnCancelar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnCancelar.Name = "btnCancelar";
-            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.Size = new System.Drawing.Size(100, 28);
             this.btnCancelar.TabIndex = 9;
             this.btnCancelar.Text = "Cancelar";
             this.btnCancelar.UseVisualStyleBackColor = true;
@@ -237,9 +254,10 @@
             // 
             this.btnGuardar.Enabled = false;
             this.btnGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnGuardar.Location = new System.Drawing.Point(300, 145);
+            this.btnGuardar.Location = new System.Drawing.Point(400, 178);
+            this.btnGuardar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnGuardar.Name = "btnGuardar";
-            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.Size = new System.Drawing.Size(100, 28);
             this.btnGuardar.TabIndex = 8;
             this.btnGuardar.Text = "Guardar";
             this.btnGuardar.UseVisualStyleBackColor = true;
@@ -248,9 +266,10 @@
             // 
             this.btnEditar.Enabled = false;
             this.btnEditar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEditar.Location = new System.Drawing.Point(171, 145);
+            this.btnEditar.Location = new System.Drawing.Point(228, 178);
+            this.btnEditar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnEditar.Name = "btnEditar";
-            this.btnEditar.Size = new System.Drawing.Size(75, 23);
+            this.btnEditar.Size = new System.Drawing.Size(100, 28);
             this.btnEditar.TabIndex = 7;
             this.btnEditar.Text = "Editar";
             this.btnEditar.UseVisualStyleBackColor = true;
@@ -265,9 +284,11 @@
             this.grpboxCategoria.Controls.Add(this.btnGuardar);
             this.grpboxCategoria.Controls.Add(this.btnEditar);
             this.grpboxCategoria.Controls.Add(this.btnNuevo);
-            this.grpboxCategoria.Location = new System.Drawing.Point(20, 26);
+            this.grpboxCategoria.Location = new System.Drawing.Point(27, 32);
+            this.grpboxCategoria.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.grpboxCategoria.Name = "grpboxCategoria";
-            this.grpboxCategoria.Size = new System.Drawing.Size(624, 197);
+            this.grpboxCategoria.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.grpboxCategoria.Size = new System.Drawing.Size(832, 242);
             this.grpboxCategoria.TabIndex = 0;
             this.grpboxCategoria.TabStop = false;
             this.grpboxCategoria.Text = "Unidades";
@@ -277,9 +298,10 @@
             this.tabConfiguracion.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.tabConfiguracion.Controls.Add(this.grpboxCategoria);
             this.tabConfiguracion.Location = new System.Drawing.Point(4, 29);
+            this.tabConfiguracion.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabConfiguracion.Name = "tabConfiguracion";
-            this.tabConfiguracion.Padding = new System.Windows.Forms.Padding(3);
-            this.tabConfiguracion.Size = new System.Drawing.Size(668, 249);
+            this.tabConfiguracion.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabConfiguracion.Size = new System.Drawing.Size(893, 314);
             this.tabConfiguracion.TabIndex = 1;
             this.tabConfiguracion.Text = "Configuración";
             this.tabConfiguracion.UseVisualStyleBackColor = true;
@@ -292,11 +314,12 @@
             this.tabControl1.Controls.Add(this.tabLista);
             this.tabControl1.Controls.Add(this.tabConfiguracion);
             this.tabControl1.ItemSize = new System.Drawing.Size(45, 25);
-            this.tabControl1.Location = new System.Drawing.Point(23, 66);
-            this.tabControl1.MaximumSize = new System.Drawing.Size(1500, 900);
+            this.tabControl1.Location = new System.Drawing.Point(31, 81);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabControl1.MaximumSize = new System.Drawing.Size(2000, 1108);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(676, 282);
+            this.tabControl1.Size = new System.Drawing.Size(901, 347);
             this.tabControl1.TabIndex = 3;
             // 
             // errorIcono
@@ -312,20 +335,34 @@
             this.lblUnidades.ForeColor = System.Drawing.Color.Tomato;
             this.lblUnidades.Image = global::Capa_Presentacion.Properties.Resources.fondo;
             this.lblUnidades.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.lblUnidades.Location = new System.Drawing.Point(23, 9);
+            this.lblUnidades.Location = new System.Drawing.Point(31, 11);
+            this.lblUnidades.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblUnidades.Name = "lblUnidades";
-            this.lblUnidades.Size = new System.Drawing.Size(111, 47);
+            this.lblUnidades.Size = new System.Drawing.Size(133, 57);
             this.lblUnidades.TabIndex = 2;
             this.lblUnidades.Text = "Unidades";
             // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(447, 2);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "LISTA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
             // FrmUnidades
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Capa_Presentacion.Properties.Resources.fondo1;
-            this.ClientSize = new System.Drawing.Size(724, 386);
+            this.ClientSize = new System.Drawing.Size(965, 475);
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.lblUnidades);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FrmUnidades";
             this.Text = "FrmUnidades";
             this.Load += new System.EventHandler(this.FrmUnidades_Load);
@@ -368,5 +405,6 @@
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.ErrorProvider errorIcono;
         private System.Windows.Forms.Label lblUnidades;
+        private System.Windows.Forms.Button button1;
     }
 }
